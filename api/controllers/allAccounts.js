5236fae4621622db3a31d6f9870d85c13da6e95d
Gaharana <mr.priyanshgaharana98@gmail.com>
2020-08-28 21:19:43 +0530

    Sort Data

--- api/controllers/allAccounts.js
+++ api/controllers/allAccounts.js
@@ -2,7 +2,7 @@ const Account = require('../models/Accounts');
 
 module.exports = async (req, res, next) => {
   try {
-    const allAccounts = await Account.find();
+    const allAccounts = await Account.find().sort({accountno: 1});
     res.json(allAccounts);
   } catch (err) {
     next(err);
