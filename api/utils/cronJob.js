5b31e1cec1da470bb7712c8809c174420fef9d9f
Gaharana <mr.priyanshgaharana98@gmail.com>
2020-08-29 04:47:17 +0530

    adjusting time

--- api/utils/cronJob.js
+++ api/utils/cronJob.js
@@ -4,11 +4,13 @@ const mailContent = require('./mailContent');
 
 module.exports = async () => {
   const date = new Date();
+  console.log(date);
   const day = date.getDate();
   const month = date.getMonth() + 1;
   const year = date.getFullYear();
   const todaysDate = `${year}-${month < 10 ? `0${month}` : month}-${day < 10 ? `0${day}` : day}`;
   const todaysAccount = await Accounts.find({ maturityDate: new Date(todaysDate) });
   if (todaysAccount.length) await sendMail(mailContent(todaysAccount));
+  console.log(todaysAccount);
   console.log('running a task every minute');
 };
