1ac66abdff973e1efde3ad283dc18ec71a73f50a
mrpriyansh <mr.priyanshgaharana@gmail.com>
2021-08-04 21:56:01 +0530

    Fix delete api

--- api/controllers/deleteAccount.js
+++ api/controllers/deleteAccount.js
@@ -3,6 +3,7 @@ const Accounts = require('../models/Accounts');
 
 module.exports = async (req, res, next) => {
   try {
+    if (!req.body.accountno) throw new ErrorHandler(400, 'Account Number is required');
     await Accounts.deleteOne({ accountno: req.body.accountno });
     res.json('Account Deleted Successfully!');
   } catch (err) {
