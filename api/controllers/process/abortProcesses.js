8841e086454ac03aebab350133ee0ffecd2d447b
Priyansh Gaharana <gaharanap@google.com>
2023-01-22 13:17:14 +0530

    Add wait time

--- api/controllers/process/abortProcesses.js
+++ api/controllers/process/abortProcesses.js
@@ -3,14 +3,16 @@ const Task = require('../../models/Task');
 module.exports = async (req, res, next) => {
   try {
     const { id, processIds } = req.body;
-    processIds.forEach(pid => {
+    // eslint-disable-next-line
+    for await (const pid of processIds) {
       try {
-        process.kill(pid);
+        await process.kill(pid);
+        await new Promise(resolve => setTimeout(resolve, 500));
       } catch (err) {
         // eslint-disable-next-line no-console
         console.log(err);
       }
-    });
+    }
     await Task.updateOne({ _id: id }, { status: 'Aborted', error: 'Aborted by you' });
     res.json('Aborted!!!');
   } catch (err) {
