d870c59b16b0b2820e63cceca52bb3e429b0ec4e
mrpriyansh <mr.priyanshgaharana@gmail.com>
2023-04-26 09:45:05 +0530

    Add auto versioning while deployment

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,3 +1,6 @@
+echo $1
+yarn version --$1
+git push
 docker build . -t gcr.io/seedbox-299206/poaa-api
 docker push gcr.io/seedbox-299206/poaa-api
 gcloud run deploy poaa-api --image gcr.io/seedbox-299206/poaa-api
