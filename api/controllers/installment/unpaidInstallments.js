effbe522c869de5a8155462936bcf5659e8006be
unknown <mr.priyanshgaharana@gmail.com>
2023-05-09 10:57:55 +0530

    Fix unpaid installment query

--- api/controllers/installment/unpaidInstallments.js
+++ api/controllers/installment/unpaidInstallments.js
@@ -1,5 +1,5 @@
 const Installment = require('../../models/Installment');
-const { INSTALLMENT_LOGGED } = require('../../utils/constants');
+const { INSTALLMENT_LOGGED, INSTALLMENT_PENDING } = require('../../utils/constants');
 
 module.exports = async (req, res, next) => {
   try {
@@ -7,8 +7,22 @@ module.exports = async (req, res, next) => {
     const currentMonth = new Date(today.getFullYear(), today.getMonth(), 1);
     const allInstallments = await Installment.find({
       agentId: req.user.id,
-      $expr: [{ status: INSTALLMENT_LOGGED }, { $gt: ['updatedAt', currentMonth] }],
+
+      $expr: {
+        $not: {
+          $and: [
+            { $gt: ['$updatedAt', currentMonth] },
+            {
+              $or: [
+                { $eq: ['$status', INSTALLMENT_LOGGED] },
+                { $eq: ['$status', INSTALLMENT_PENDING] },
+              ],
+            },
+          ],
+        },
+      },
     }).sort({ name: 1 });
+    console.log(allInstallments.length);
     res.json(allInstallments);
   } catch (err) {
     next(err);
