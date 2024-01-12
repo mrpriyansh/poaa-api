428486cbe8540b973d1ddc610b9e1bab158b15a9
unknown <mr.priyanshgaharana@gmail.com>
2024-01-12 20:19:52 +0530

    Add name for signup

--- api/controllers/user/signup.js
+++ api/controllers/user/signup.js
@@ -4,16 +4,18 @@ const { ErrorHandler } = require('../../../services/handleError');
 
 const handleSignUp = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
+    const { email, password, name } = req.body;
     if (!email || !password) {
       throw new ErrorHandler(400, 'Fields Cannot be empty');
     }
 
-    const exists = await User.findOne({ email: email.toLowerCase() });
+    const loweredCaseEmail = email.toLowerCase();
+
+    const exists = await User.findOne({ email: loweredCaseEmail });
     if (exists) throw new ErrorHandler(409, 'Email already Exists');
 
     const hash = await bcrypt.hash(password, 10);
-    await User.create({ email, password: hash });
+    await User.create({ email: loweredCaseEmail, password: hash, name });
     res.status(200).json({ icon: 'success', title: 'Account Created Successfully' });
   } catch (err) {
     next(err);
--- api/models/User.js
+++ api/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const User = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
