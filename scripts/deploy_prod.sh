4dad46b8c57c713ace92eccf1df3466b566ccf7b
unknown <mr.priyanshgaharana@gmail.com>
2025-04-04 07:15:26 +0530

    Update deployment instructions

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,6 +3,6 @@ git checkout master
 git pull origin master
 yarn version --$1
 git push
-docker build . -t gcr.io/seedbox-299206/poaa-api
-docker push gcr.io/seedbox-299206/poaa-api
-gcloud run deploy poaa-api --image gcr.io/seedbox-299206/poaa-api --set-env-vars "NODE_ENV=production"
+docker build . -t us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
+docker push us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
+gcloud run deploy poaa-api --image us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest   --set-env-vars "NODE_ENV=production"
