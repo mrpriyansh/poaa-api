6966b7bd2ece867bded2ab6076b1ad90eca59d89
unknown <mr.priyanshgaharana@gmail.com>
2025-04-06 21:22:27 +0530

    Update the deployment script

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,6 +3,6 @@ git checkout master
 git pull origin master
 yarn version --$1
 git push
-docker build . -t us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
-docker push us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
-gcloud run deploy poaa-api --image us-east1-docker.pkg.dev/poaa-389702/api/poaa-api:latest   --set-env-vars "NODE_ENV=production"
+docker build . -t northamerica-northeast1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
+docker push northamerica-northeast1-docker.pkg.dev/poaa-389702/api/poaa-api:latest  
+gcloud run deploy poaa-api --image northamerica-northeast1-docker.pkg.dev/poaa-389702/api/poaa-api:latest   --set-env-vars "NODE_ENV=production"
