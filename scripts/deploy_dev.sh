03121e2d42ad96056d93d7b77ec3a80657c3ad93
unknown <mr.priyanshgaharana@gmail.com>
2026-06-17 08:25:46 +0530

    Convert deploy scripts to JS, remove obsolete shell scripts

--- scripts/deploy_dev.sh
+++
@@ -1,4 +0,0 @@
-docker container kill poaa-api
-docker container remove poaa-api
-docker build . -t northamerica-northeast1-docker.pkg.dev/poaa-389702/api/poaa-api:latest
-docker run -d -p 4000:8080 --env NODE_ENV=development --name poaa-api northamerica-northeast1-docker.pkg.dev/poaa-389702/api/poaa-api:latest
